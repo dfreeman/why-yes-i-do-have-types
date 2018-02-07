6dbb8611a3eda37b3c1409e26e287f2883b4c6e0
Dan Freeman <dfreeman@salsify.com>
2018-02-06 23:53:02 -0500

    Better hooks

--- tests/dummy/app/app.ts
+++ tests/dummy/app/app.ts
@@ -2,8 +2,8 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-import { sayHello } from 'why-yes-i-do-have-types';
-import join from 'why-yes-i-do-have-types/utils/join';
+import { sayHello } from '@dfreeman/why-yes-i-do-have-types';
+import join from '@dfreeman/why-yes-i-do-have-types/utils/join';
 
 console.log(sayHello(join(['W', 'o', 'r', 'l', 'd'])));
 
